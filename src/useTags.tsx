a9fe3c6fd8638ea7be64a63cfca3bdcf4fdec293
heeeyueee <heeeyueee@163.com>
2021-07-11 16:46:01 +0800

    faet: 标签页面 编辑标签 删除标签 返回

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -2,16 +2,40 @@ import { createId } from 'lib/createId';
 import { useState } from 'react';
 
 const defaultTags = [
-    {id: createId(), name: '衣'},
-    {id: createId(), name: '食'},
-    {id: createId(), name: '住'},
-    {id: createId(), name: '行'},
-  ];
+    { id: createId(), name: '衣' },
+    { id: createId(), name: '食' },
+    { id: createId(), name: '住' },
+    { id: createId(), name: '行' },
+];
 
 const useTags = () => {
     const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
-    const findTag =(id:number)=>tags.filter(tag=>tag.id===id)[0]
-    return { tags, setTags, findTag};
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0]
+    const findTagIndex = (id: number) => {
+        let result = -1
+        for (let index = 0; index < tags.length; index++) {
+            if (tags[index].id === id) {
+                result = index
+                break
+            }
+            return result
+        }
+    }
+    const editTagContent = (id: number, obj: { name: string }) => {
+        const index = findTagIndex(id)
+        let cloneTags = JSON.parse(JSON.stringify(tags))
+        cloneTags.splice(index, 1, { id: id, name: obj.name })
+        setTags(cloneTags)
+
+    }
+    const deleteTag = (id: number) => {
+        const index = findTagIndex(id)
+        let cloneTags = JSON.parse(JSON.stringify(tags))
+        cloneTags.splice(index, 1)
+        setTags(cloneTags)
+
+    }
+    return { tags, setTags, findTag, findTagIndex, editTagContent, deleteTag };
 };
 
 export { useTags };
--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,34 +1,23 @@
 import styled from "styled-components"
-import React from 'react';
+import React, { ChangeEventHandler } from 'react';
+import { Input } from '../../components/Input';
 const Wrapper = styled.section`
 background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;
-  > label {
-    display:flex;
-    align-items: center;
-    > span { margin-right: 16px; white-space: nowrap; }
-    > input {
-      display:block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }`;
+  `;
 type Props = {
   value: string;
   onChange: (value: string) => void;
 }
 const NoteSection: React.FunctionComponent<Props> = (props) => {
+  const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
+  };
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text"
-          value={props.value}
-          onChange={(e) => { props.onChange(e.target.value) }} />
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange} placeholder="请填写备注" />
     </Wrapper>
   )
 }
